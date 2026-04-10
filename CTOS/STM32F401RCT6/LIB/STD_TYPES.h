736716c806672c5778da1ab7970bdd3b45d3748a
Khaled Elwan <khaledelwan2004@gmail.com>
2026-04-10 16:49:04 +0200

    add lib

---
+++ CTOS/STM32F401RCT6/LIB/STD_TYPES.h
@@ -0,0 +1,17 @@
+/************************************************
+ * Author: Khaled Elwan
+ * Date: 2024-06-01  
+ * Description: This file contains the standard data types definitions for the project.
+ * version: 1.0
+ ************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H 
+
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+typedef unsigned long uint32_t;
+typedef signed long int32_t;
+
+#endif /* STD_TYPES_H */
