3a280a696bc6f25e1e3fa6ee4f338d5da9141362
Khaled Elwan <khaledelwan2004@gmail.com>
2026-08-20 02:38:08 +0300

    update lib

--- CTOS/STM32F401RCT6/LIB/BIT_MATH.h
+++ CTOS/STM32F401RCT6/LIB/BIT_MATH.h
@@ -9,7 +9,7 @@
 
 
 
-#define SET_BIT(REG,BIT)     (REG |= (1 << BIT))
+#define SET_BIT(REG,BIT)     (REG |= (1 << BIT))    
 #define CLR_BIT(REG,BIT)     (REG &= ~(1 << BIT))
 #define TOG_BIT(REG,BIT)     (REG ^= (1 << BIT))
 #define GET_BIT(REG,BIT)     ((REG >> BIT) & 1) 
